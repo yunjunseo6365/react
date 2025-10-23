e69f6e70235a9d89a555f41d04baa0d872c7a8a8
yun.junseo6365 <yun.junseo6365@gmail.com>
2025-10-23 23:31:41 +0900

    25-10-23 실습과제 수정

--- travellog-app/src/components/Addcard.jsx
+++ travellog-app/src/components/Addcard.jsx
@@ -11,13 +11,21 @@ function Addcard({onCard}) {
     // 평점
     const [rating, setRating] = useState(3);
 
-    // <form>태그 내에서 submit으로 이벤트 발생했을때
+    // <form>태그 내에서 submit으로 이벤트 발생
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        // 필수 요소
+        if (title.trim() === '' || text.trim() === '') {
+            alert('여행지 이름과 설명을 입력해주세요.');
+            return;
+        }
+
         onCard(title, img, rating, text)
         setTitle('');
         setText('');
-        setImg('');
+        setRating(0);
+        setImg('https://placehold.co/300x200?text=none');
     }
 
     return (
@@ -26,7 +34,7 @@ function Addcard({onCard}) {
             <div>
                 <h3>여행지 이름</h3>
                 <input type="text" className="title" placeholder="예 : 파리" 
-                onChange={(e) => setTitle(e.target.value)} />
+                onChange={(e) => setTitle(e.target.value)} value={title} />
             </div>
             <div>
                 <h3>이미지 URL (선택사항)</h3>
@@ -41,7 +49,7 @@ function Addcard({onCard}) {
             <div>
                 <h3>평점 (1 - 5)  /  현재평점 : {rating}</h3>
                 <input type="range" min="0" max="5" className="rating-range" 
-                onChange={(e) => setRating(e.target.value)} />
+                onChange={(e) => setRating(e.target.value)} value={rating} />
             </div>
             <div>
                 <button type="submit" className="submit-trv-btn">여행지 추가</button>
