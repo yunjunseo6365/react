71454f9415c7e715caf32bb827b036e99dc41441
yun.junseo6365 <yun.junseo6365@gmail.com>
2025-10-27 19:54:27 +0900

    25-10-27 공부 내용 수정사항

--- todo-app/src/pages/NotFound.js
+++ todo-app/src/pages/NotFound.js
@@ -6,7 +6,7 @@ function NotFound() {
     <div className="notfound">
         <h1>404 - 페이지를 찾을 수 없습니다</h1>
         <p>요청하신 페이지가 존재하지 않습니다.</p>
-        <Link to='/home'>
+        <Link to='/'>
             <button className="notfound-btn">홈으로 돌아가기</button>
         </Link>
     </div>
